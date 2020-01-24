9d11a510adb618c16539befabe283203ab7e1d7d
Denis Girko <denis.girko@gmail.com>
2020-01-24 11:27:31 +0300

    Updated errors

--- errors.go
+++ errors.go
@@ -3,50 +3,60 @@ package cloudloyalty_client
 import "fmt"
 
 const (
+	ErrGeneralError            = 1
+	ErrMalformedRequest        = 2
 	ErrClientNotFound          = 3
-	ErrRequestedTooManyBonuses = 6
+	ErrClientSuspended         = 4
+	ErrShopNotFound            = 5
+	ErrIncorrectBonusAmount    = 6
+	ErrIncorrectReturnItem     = 10
+	ErrIncorrectReturnAmount   = 11
 	ErrAlreadyProcessed        = 17
 	ErrIncorrectPhone          = 20
+	ErrPurchaseNotFound        = 21
 	ErrDuplicatingPhone        = 24
-	ErrOrderProcessed          = 32
+	ErrDuplicatingCard         = 25
+	ErrTooManyCodeRequests     = 28
+	ErrEmptyPhone              = 29
+	ErrDuplicatingExternalID   = 30
+	ErrOrderNotFound           = 31
+	ErrOrderAlreadyProcessed   = 32
+	ErrPromocodeNotFound       = 33
+	ErrPromocodeNotApplicable  = 34
+	ErrPromocodeAlreadyUsed    = 35
+	ErrPromocodeExpired        = 36
+	ErrPromocodeRequiresClient = 37
 )
 
 var russianText = map[int]string{
-	1:                 "Чек не обработан процессингом или обработан с ошибкой",
-	2:                 "В запросе к процессингу обнаружена ошибка или неверный вид JSON",
-	ErrClientNotFound: "Клиент не найден",
-	4:                 "Аккаунт клиента заблокирован",
-	5:                 "Не найден магазин",
-	ErrRequestedTooManyBonuses: "Списание бонусов превышает допустимое значение",
-	10:                  "Возвращаемый товар не найден в чеке продажи",
-	11:                  "Сумма возврата больше суммы продажи",
-	13:                  "Чек продажи не найден на сервере процессинга",
-	14:                  "Чек для отмены не найден",
-	15:                  "Отмена допускается только для последней операции",
-	16:                  "Превышен допустимый интервал времени для отмены операции",
-	ErrAlreadyProcessed: "Чек с данным номером уже обработан",
-	ErrIncorrectPhone:   "Номер телефона клиента не валиден",
-	21:                  "Продажа не найдена",
-	22:                  "Не передан ключ аутентификации",
-	23:                  "Клиент не аутентифицирован",
-	ErrDuplicatingPhone: "Клиент с таким номером телефона уже существует",
-	25:                  "Карта уже привязана к другому клиенту",
-	26:                  "Код подтверждения не отправлялся",
-	27:                  "Код подтверждения некорректен",
-	28:                  "Слишком частая отправка кода подтверждения",
-	29:                  "У клиента не задан номер телефона",
-	30:                  "Клиент с таким внешним идентификатором уже существует",
-	31:                  "Заказ не найден",
-	ErrOrderProcessed:   "Заказ уже обработан",
-	33:                  "Промокод не найден",
-	34:                  "Условия промокода не выполнены",
-	35:                  "Промокод уже использован максимальное число раз",
-	36:                  "Время действия промокода еще не наступило или уже прошло",
+	ErrGeneralError:            "Чек не обработан процессингом или обработан с ошибкой",
+	ErrMalformedRequest:        "В запросе к процессингу обнаружена ошибка или неверный вид JSON",
+	ErrClientNotFound:          "Клиент не найден",
+	ErrClientSuspended:         "Аккаунт клиента заблокирован",
+	ErrShopNotFound:            "Не найден магазин",
+	ErrIncorrectBonusAmount:    "Списание бонусов превышает допустимое значение",
+	ErrIncorrectReturnItem:     "Возвращаемый товар не найден в чеке продажи",
+	ErrIncorrectReturnAmount:   "Сумма возврата больше суммы продажи",
+	ErrAlreadyProcessed:        "Чек с данным номером уже обработан",
+	ErrIncorrectPhone:          "Номер телефона клиента не валиден",
+	ErrPurchaseNotFound:        "Продажа не найдена",
+	ErrDuplicatingPhone:        "Клиент с таким номером телефона уже существует",
+	ErrDuplicatingCard:         "Карта уже привязана к другому клиенту",
+	ErrTooManyCodeRequests:     "Слишком частая отправка кода подтверждения",
+	ErrEmptyPhone:              "У клиента не задан номер телефона",
+	ErrDuplicatingExternalID:   "Клиент с таким внешним идентификатором уже существует",
+	ErrOrderNotFound:           "Заказ не найден",
+	ErrOrderAlreadyProcessed:   "Заказ уже обработан",
+	ErrPromocodeNotFound:       "Промокод не найден",
+	ErrPromocodeNotApplicable:  "Условия промокода не выполнены",
+	ErrPromocodeAlreadyUsed:    "Промокод уже использован максимальное число раз",
+	ErrPromocodeExpired:        "Время действия промокода еще не наступило или уже прошло",
+	ErrPromocodeRequiresClient: "Промокод работает только совместно с бонусным счетом",
 }
 
 func RussianDescription(code int) string {
 	if desc, ok := russianText[code]; ok {
 		return desc
 	}
-	return fmt.Sprintf("Ошибка, код #%d", code)
+	return fmt.Sprintf("Ошибка #%d", code)
 }
